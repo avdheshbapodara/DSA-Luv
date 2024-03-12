34a213dbe8e78039305117fc7bacdee3aa43d123
Avdhesh Bapodara <avdhesh.bict21@sot.pdpu.ac.in>
2024-03-12 21:11:46 +0530

    12 March 24

---
+++ 038-Upper_Bound_and_Lower_Bound_With_Binary_Search.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int upperbound(vector<int> &v, int element)
+{
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int element;
+    cin >> element;
+}
+/*
+6
+1 2 3 4 5 6
+5
+*/
